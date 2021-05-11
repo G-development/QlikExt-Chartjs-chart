a5c8d7fe16f4f66bea9288de618c986cb8ea27c9
Giovanni <62568244+G-development@users.noreply.github.com>
2021-05-11 12:28:36 +0200

    v0.3

--- hist_chart.js
+++ hist_chart.js
@@ -5,12 +5,12 @@ define([
     './initial',
     './js/chart'
 ],
-    function ($, props, i, chart) {
+    function ($, props) {
         'use strict';
-
+        var datasets = [];
         return {
 
-            //def of layout-panels - ref to properties.js
+            //def of layout-panels - ref to properties.js / initial.js
             definition: props,
             initialProperties: initial,
 
@@ -19,7 +19,8 @@ define([
             paint: function ($element, layout) {
                 //Create hyperCube var
                 var hc = layout.qHyperCube;
-                //console.log('Data returned:', hc);
+                console.log("Layout:", layout);
+                console.log("Data returned:", hc);
 
                 //Empty the element
                 $element.empty();
@@ -27,22 +28,21 @@ define([
                 // Create datasets
                 var dataLabels = hc.qDataPages[0].qMatrix.map(function (d) { return d[0].qText; })
 
-                var datasets = [];
-                var cols = new Array(layout.props.firstDataset.color, layout.props.secondDataset.color, layout.props.thirdDataset.color);
+                datasets = [];
+                var colors = new Array(layout.props.firstDataset.color, layout.props.secondDataset.color, layout.props.thirdDataset.color, layout.props.fourthDataset.color, layout.props.fifthDataset.color);
                 for (var i = 0; i < hc.qMeasureInfo.length; i++) {
                     datasets.push(
                         {
                             label: hc.qMeasureInfo[i].qFallbackTitle,
-                            backgroundColor: cols[i],
+                            backgroundColor: colors[i],
                             fill: 'origin',
                             data: hc.qDataPages[0].qMatrix.map(function (d) { return { y: d[i + 1].qNum } }),
                             stack: 'Stack' + i
                         }
-                    )               
+                    )
                 }
 
-
-                //If props.stacked = true, then stack the bars, else don't
+                //If props.stacked = true then stack the bars, else don't
                 if (layout.props.stacked === true) {
                     for (let i = 0; i < hc.qMeasureInfo.length; i++) {
                         datasets[i].stack = 'Uno';
@@ -52,6 +52,13 @@ define([
                         datasets[i].stack = i;
                     }
                 }
+
+                //If props.randBtn is clicked, randomize colors
+                if (layout.props.randBtn) {
+                    console.log(datasets[i].backgroundColor);
+                }
+
+
 
                 //CHART
                 var canvas_id = layout.qInfo.qId + "_chartjs_bar";
@@ -88,11 +95,30 @@ define([
                             },
                             y: {
                                 stacked: true
-                            }
+                            },
+                            xAxes: [{
+                                gridLines: {
+                                    display: true
+                                }
+                            }],
+                            yAxes: [{
+                                gridLines: {
+                                    display: true
+                                }
+                            }]
                         }
                     }
 
                 });
+
+                //If props.grid = true, then enable the grid, else don't
+                if (layout.props.grid === true) {
+                    myChart.options.scales.xAxes[0].gridLines.display = true;
+                    myChart.options.scales.yAxes[0].gridLines.display = true;
+                } else {
+                    myChart.options.scales.xAxes[0].gridLines.display = false;
+                    myChart.options.scales.yAxes[0].gridLines.display = false;
+                }
 
 
                 console.log('------------ END ------------');
--- properties.js
+++ properties.js
@@ -7,127 +7,182 @@ function getRandomColor() {
     return color;
 }
 
-define([], function () {
-    'use strict';
+define([
+    './hist_chart'
+],
+    function () {
+        'use strict';
 
-    //Dimensions - measures - sortings
-    var dimensions = {
-        uses: "dimensions",
-        min: 1,
-        max: 1
-    };
+        //Dimensions - measures - sortings
+        var dimensions = {
+            uses: "dimensions",
+            min: 1,
+            max: 1
+        };
 
-    var measures = {
-        uses: "measures",
-        min: 1
-    };
+        var measures = {
+            uses: "measures",
+            min: 1,
+            max: 5
+        };
 
-    var sorting = {
-        uses: "sorting"
-    }
-
-    //myText
-    /*
-    var myText = {
-        ref: "prop.myText",
-        type: "string",
-        label: "Insert text",
-        defaultValue: "#3C887E"
-    }
-    */
-
-    //color-pickers
-    var colorPicks = [
-        {
-            label: "First dataset color",
-            component: "color-picker",
-            ref: "props.firstDataset",
-            type: "object",
-            defaultValue: {
-                index: "-1",
-                color: getRandomColor()
-            }
-        },
-        {
-            label: "Second dataset color",
-            component: "color-picker",
-            ref: "props.secondDataset",
-            type: "object",
-            defaultValue: {
-                index: "-1",
-                color: getRandomColor()
-            }
-        },
-        {
-            label: "Third dataset color",
-            component: "color-picker",
-            ref: "props.thirdDataset",
-            type: "object",
-            defaultValue: {
-                index: "-1",
-                color: getRandomColor()
-            }
+        var sorting = {
+            uses: "sorting"
         }
-    ]
 
-    var checkBox = {
-        type: "boolean",
-        label: "Stacked",
-        ref: "props.stacked",
-        defaultValue: false
-    }
+        //myText
+        /*
+        var myText = {
+            ref: "prop.myText",
+            type: "string",
+            label: "Insert text",
+            defaultValue: "#3C887E"
+        }
+        */
 
-
-    //Appearance section
-    var appearanceSection = {
-        uses: "settings",
-        items: {
-            //Colors panel
-            colors: {
-                type: "items",
-                label: "Colors",
-                items: {
-                    //myText: myText,
-                    //cP1: colorPicks[0],
-                    //cP2: colorPicks[1],
-                    //cP3: colorPicks[2]
+        //color-pickers
+        var colorPicks = [
+            {
+                label: "First dataset color",
+                component: "color-picker",
+                ref: "props.firstDataset",
+                type: "object",
+                defaultValue: {
+                    index: "-1",
+                    color: getRandomColor()
                 }
             },
-            //settings panel
-            settings: {
-                type: "items",
-                label: "Settings",
-                items: {
-                    checkStacked: checkBox
+            {
+                label: "Second dataset color",
+                component: "color-picker",
+                ref: "props.secondDataset",
+                type: "object",
+                defaultValue: {
+                    index: "-1",
+                    color: getRandomColor()
+                }
+            },
+            {
+                label: "Third dataset color",
+                component: "color-picker",
+                ref: "props.thirdDataset",
+                type: "object",
+                defaultValue: {
+                    index: "-1",
+                    color: getRandomColor()
+                }
+            },
+            {
+                label: "Fourth dataset color",
+                component: "color-picker",
+                ref: "props.fourthDataset",
+                type: "object",
+                defaultValue: {
+                    index: "-1",
+                    color: getRandomColor()
+                }
+            },
+            {
+                label: "Fifth dataset color",
+                component: "color-picker",
+                ref: "props.fifthDataset",
+                type: "object",
+                defaultValue: {
+                    index: "-1",
+                    color: getRandomColor()
                 }
             }
+        ]
 
-
+        var checkGrid = {
+            type: "boolean",
+            component: "switch",
+            label: "Grid",
+            ref: "props.grid",
+            options: [{
+                value: true,
+                label: "On"
+            }, {
+                value: false,
+                label: "Off"
+            }],
+            defaultValue: true
         }
-    };
 
-    var nomeCP = ["cP1", "cP2", "cP3"];
-
-    for (var i=0; i<3; i++) {
-        var x = appearanceSection.items.colors.items;
-        appearanceSection.items.colors.items.cP1 = colorPicks[i-2];
-        appearanceSection.items.colors.items.cP2 = colorPicks[i-1];
-        appearanceSection.items.colors.items.cP3 = colorPicks[i];
-
-    };
-
-    // Main property panel definition
-    // Only what's defined here will be returned from properties.js
-    return {
-        type: "items",
-        component: "accordion",
-        items: {
-            dimensions: dimensions,
-            measures: measures,
-            sorting: sorting,
-            appearancePanel: appearanceSection
+        var checkStacked = {
+            type: "boolean",
+            component: "switch",
+            label: "Stack bars",
+            ref: "props.stacked",
+            options: [{
+                value: true,
+                label: "Stacked"
+            }, {
+                value: false,
+                label: "Not stacked"
+            }],
+            defaultValue: false
         }
-    };
 
-});
+        var randomizeBtn = {
+            label: "Randomize colors",
+            component: "button",
+            ref:"props.randBtn",
+            action: function () {
+                for(var i=0; i<5; i++) {
+                    
+                }
+            }
+        }
+
+        //Appearance section
+        var appearanceSection = {
+            uses: "settings",
+            items: {
+                //Colors panel
+                colors: {
+                    type: "items",
+                    label: "Colors",
+                    items: {
+                        //myText: myText,
+                        //cP1: colorPicks[0],
+                        //cP2: colorPicks[1],
+                        //cP3: colorPicks[2]
+                    }
+                },
+                //settings panel
+                settings: {
+                    type: "items",
+                    label: "Settings",
+                    items: {
+                        checkGrid: checkGrid,
+                        checkStacked: checkStacked,
+                        randomizeBtn: randomizeBtn
+                    }
+                }
+
+
+            }
+        };
+
+        //Display colorPick according on number of measures
+        //Missing condition
+        for (var i = 0; i < 5; i++) {
+            appearanceSection.items.colors.items["cP" + i] = colorPicks[i];
+        };
+
+
+        // Main property panel definition
+        return {
+            type: "items",
+            component: "accordion",
+            items: {
+                dimensions: dimensions,
+                measures: measures,
+                sorting: sorting,
+                appearancePanel: appearanceSection
+            }
+        };
+
+    });
 
